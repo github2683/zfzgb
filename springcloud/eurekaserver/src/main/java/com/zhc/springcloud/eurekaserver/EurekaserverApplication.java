50dade8cd175ddc9400c36baae5a8c544f4876b1
zfz <1009600394@qq.com>
2018-07-03 17:59:10 +0800

    创建Eureka注册服务中心成功

--- springcloud/eurekaserver/src/main/java/com/zhc/springcloud/eurekaserver/EurekaserverApplication.java
+++ springcloud/eurekaserver/src/main/java/com/zhc/springcloud/eurekaserver/EurekaserverApplication.java
@@ -2,7 +2,9 @@ package com.zhc.springcloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer //标注为注册服务中心
 @SpringBootApplication
 public class EurekaserverApplication {
 
