73d9b9c3b3a234b970077aa72bebb66acda4dbb2
zfz <1009600394@qq.com>
2018-07-04 18:16:58 +0800

    增加熔断器和监控仪表

--- feignservice/src/main/java/com/zhc/springcloud/feignservice/remotecall/ServiceClient1Remote.java
+++ feignservice/src/main/java/com/zhc/springcloud/feignservice/remotecall/ServiceClient1Remote.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-client1")
+@FeignClient(value = "service-client1",fallback = RemoteHystric.class)
 public interface ServiceClient1Remote {
 
     @RequestMapping(value = "/user/getName",method = RequestMethod.GET)
